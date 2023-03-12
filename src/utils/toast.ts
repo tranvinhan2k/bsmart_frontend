3ee35d0751688176b554b0ef8e8d76b78572bf12
tranvinhan <tranvinhan2k@gmail.com>
2023-03-12 14:27:33 +0700

    update close button for toast

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -5,11 +5,33 @@ const loadToast = (label: string) => {
 }
 
 const updateSuccessToast = (id: Id, label: string) => {
+<<<<<<< Updated upstream
     toast.update(id, { render: label, type: 'success', isLoading: false, autoClose: 5000 })
 }
 const updateFailedToast = (id: Id, label: string) => {
     toast.update(id, { render: label, type: 'error', isLoading: false, closeOnClick: true })
 }
+=======
+  toast.update(id, {
+    render: label,
+    type: 'success',
+    isLoading: false,
+    closeOnClick: true,
+    closeButton: true,
+    autoClose: 5000,
+  });
+};
+const updateFailedToast = (id: Id, label: string) => {
+  toast.update(id, {
+    render: label,
+    type: 'error',
+    isLoading: false,
+    closeOnClick: true,
+    closeButton: true,
+    autoClose: 10000,
+  });
+};
+>>>>>>> Stashed changes
 
 export default {
     loadToast,
