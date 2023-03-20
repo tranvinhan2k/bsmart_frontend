0c9cdffb50752907c2968e5de26fe6f6b29ddfed
tranvinhan <tranvinhan2k@gmail.com>
2023-03-21 06:11:27 +0700

    add css for library and config local variable

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,15 @@ import { ToastContainer } from 'react-toastify';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import store from '~/redux/store';
 import defaultTheme from '~/themes';
-import './App.css';
 import MainLayout from '~/layouts/MainLayout';
 import routes from '~/routes';
 import { RoutePayload } from '~/models/routes';
 import LazyLoadingScreen from '~/components/atoms/LazyLoadingScreen';
+// css
+import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css';
+import localEnvironment from './utils/localEnvironment';
 
 const showRoutes = () => {
   let result = null;
@@ -54,7 +57,7 @@ const queryClient = new QueryClient();
 
 function Wrapper() {
   return (
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_KEY}>
+    <GoogleOAuthProvider clientId={localEnvironment.GOOGLE_CLIENT_KEY}>
       <QueryClientProvider client={queryClient}>
         <Provider store={store}>
           <ThemeProvider theme={defaultTheme}>
