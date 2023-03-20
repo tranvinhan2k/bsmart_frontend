3246949e55c625fbb4227aac0c0fb56b0e2a4c7f
tranvinhan <tranvinhan2k@gmail.com>
2023-03-21 06:10:08 +0700

    add api mentor data

---
+++ src/api/mentorProfiles.ts
@@ -0,0 +1,70 @@
+import axiosClient from '~/api/axiosClient';
+import { MentorPayload } from '~/models/mentor';
+
+const url = `/mentorProfiles`;
+
+export interface ResponseMentorData {
+  id: number;
+  introduce: string;
+  yearsOfExperience: string;
+  user: {
+    id: number;
+    username: string;
+    password: string;
+    fullName: string;
+    email: string;
+    birthday: '2023-03-19T03:47:56.077Z';
+    address: string;
+    phone: string;
+    status: true;
+    roles: [
+      {
+        id: number;
+        name: string;
+        code: 'TEACHER';
+      }
+    ];
+    twitterLink: string;
+    facebookLink: string;
+    instagramLink: string;
+    userImages: [
+      {
+        id: number;
+        name: string;
+        url: string;
+      }
+    ];
+    wallet: {
+      id: number;
+      balance: number;
+      previous_balance: number;
+      owner_id: number;
+    };
+  };
+  skillList: [
+    {
+      id: number;
+      code: string;
+      name: string;
+      categoryId: number;
+    }
+  ];
+}
+
+function handleGetMentorById(data: ResponseMentorData) {
+  const result: MentorPayload = {
+    id: data.id,
+    avatar: data.user?.userImages?.[0].url,
+    name: data.user?.fullName,
+  };
+  return result;
+}
+
+const mentorProfileApi = {
+  async getMentorById(id: number): Promise<MentorPayload | undefined> {
+    const response: ResponseMentorData = await axiosClient.get(`${url}/${id}`);
+    return handleGetMentorById(response);
+  },
+};
+
+export default mentorProfileApi;
