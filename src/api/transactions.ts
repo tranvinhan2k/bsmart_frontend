46632d4b4cd7adcc74492c2ac4b3d2d232d70fe2
tranvinhan <tranvinhan2k@gmail.com>
2023-03-23 15:28:53 +0700

    add pay transaction api

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -13,6 +13,15 @@ const transactionsApi = {
   async payQuick(subCourseId: number): Promise<any> {
     return axiosClient.post(`${url}/pay-quick`, { subCourseId });
   },
+  async pay(cartItemId: number): Promise<any> {
+    return axiosClient.post(`${url}/pay`, { cartItemId });
+  },
+  async getTransactions(): Promise<any> {
+    return axiosClient.post(`${url}`);
+  },
+  async DepositMoney(): Promise<any> {
+    return axiosClient.post(`${url}/deposit`, { amount: 100000000 });
+  },
 };
 
 export default transactionsApi;
