b96a53a121368f8bb0a47807382ae6e578fcb23d
tranvinhan <tranvinhan2k@gmail.com>
2023-03-29 08:21:54 +0700

    navigate login page when user access authorized page

--- src/pages/AuthorizePage/index.tsx
+++ src/pages/AuthorizePage/index.tsx
@@ -1,12 +1,7 @@
 import { Stack, Typography } from '@mui/material';
+import { Navigate } from 'react-router-dom';
 import { SX_NOT_FOUND_STACK, SX_NOT_FOUND_TEXT } from './styles';
 
 export default function AuthorizePage() {
-  return (
-    <Stack sx={SX_NOT_FOUND_STACK}>
-      <Typography sx={SX_NOT_FOUND_TEXT}>
-        {`Bạn không có quyền truy cập trang này `}
-      </Typography>
-    </Stack>
-  );
+  return <Navigate to="/login" />;
 }
