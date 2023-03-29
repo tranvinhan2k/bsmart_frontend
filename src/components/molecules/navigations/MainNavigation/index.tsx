d53e52bfe590a2f7de553f5a2716fc7e466f6a5c
tranvinhan <tranvinhan2k@gmail.com>
2023-03-29 08:05:14 +0700

    update main Navigation for display menu modal

--- src/components/molecules/navigations/MainNavigation/index.tsx
+++ src/components/molecules/navigations/MainNavigation/index.tsx
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import {
   IconButton,
   Stack,
@@ -5,91 +9,99 @@ import {
   Menu,
   MenuItem,
   Badge,
+  Button,
+  Drawer,
 } from '@mui/material';
 
-import { NavLink, useNavigate } from 'react-router-dom';
-import React, { useState, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Drawer from '@mui/material/Drawer';
 import { Color, FontFamily, FontSize, MetricSize } from '~/assets/variables';
-import { ActionPayload, ContractPayload, SocialPayload } from '~/models';
-import { SX_NAVIGATION_STACK } from './styles';
+import {
+  ActionPayload,
+  ContractPayload,
+  PagingFilterPayload,
+  SocialPayload,
+} from '~/models';
+
 import Icon from '~/components/atoms/Icon';
 import SearchBar from '~/components/atoms/SearchBar';
 import SocialBar from '~/components/molecules/SocialBar';
 import ContractBar from '~/components/molecules/ContractBar';
-import { AuthorizationActionData } from '~/constants';
 import AuthorizationBar from '../../MainHeader/AuthorizationBar';
-import localEnvironment from '~/utils/localEnvironment';
-import { selectIsToggleAddToCart, selectRole } from '~/redux/user/selector';
-import { useQueryGetCart } from '~/hooks';
-import { selectFilterParams } from '~/redux/courses/selector';
-import { toggleAddToCart } from '~/redux/user/slice';
+
+import { AuthorizationActionData } from '~/constants';
+
+import styles from './styles';
+import { Role } from '~/models/role';
+import { CoursePayload } from '~/models/courses';
+import { ResponseCartItem } from '~/api/cart';
+import { MentorPayload } from '~/models/mentor';
 
 interface NavigationProps {
+  texts: {
+    CART_LINK: string;
+    INTRODUCE_MENTOR_KEYWORD: string;
+    COURSE_MENU_KEYWORD: string;
+    COURSE_MENU_LINK: string;
+    MOCK_TEACHER_NAME: string;
+    MOCK_COURSE_NAME: string;
+    APP_NAME: string;
+    SEARCH_COURSE_PLACEHOLDER: string;
+  };
+  isOpenDrawer: boolean;
+  cart: ResponseCartItem | undefined;
+  mentors: MentorPayload[] | undefined;
+  courses: CoursePayload[] | undefined;
+  role: Role | null;
+  filterParams: {
+    q: string | undefined;
+    categoryId: number[] | undefined;
+    subjectId: number[] | undefined;
+    page: number;
+    size: number;
+    sort: string[] | undefined;
+    provinces: number[] | undefined;
+    types: number[] | undefined;
+  } | null;
   pathName: string;
   pages: ActionPayload[];
   socials: SocialPayload[];
   contracts: ContractPayload[];
+  mentorAnchorEl: HTMLElement | null;
+  courseAnchorEl: HTMLElement | null;
+  onCloseMentorMenu: () => void;
+  onToggleDrawer: () => void;
+  onNavigationLink: (_link: string) => void;
+  onCloseCourseMenu: () => void;
   onSearchCourse: (searchValue: string) => void;
+  onMouseEnterNavigation: (_event: any, _link: string) => void;
+  onMouseLeaveNavigation: () => void;
+  onClickNavigation: (_link: string) => void;
+  onClickCart: () => void;
 }
 
 export default function MainNavigation({
+  texts,
+  isOpenDrawer,
+  cart,
+  mentors,
+  courses,
+  role,
+  filterParams,
   pathName,
   pages,
   contracts,
   socials,
+  courseAnchorEl,
+  mentorAnchorEl,
+  onCloseCourseMenu,
+  onCloseMentorMenu,
+  onToggleDrawer,
+  onNavigationLink,
   onSearchCourse,
+  onClickCart,
+  onClickNavigation,
+  onMouseEnterNavigation,
+  onMouseLeaveNavigation,
 }: NavigationProps) {
-  const navigation = useNavigate();
-  const [isOpenDrawer, setOpenDrawer] = useState<boolean>(false);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
-  const dispatch = useDispatch();
-  const { cart, refetch } = useQueryGetCart();
-  const role = useSelector(selectRole);
-  const filterParams = useSelector(selectFilterParams);
-  const selectAddToCart = useSelector(selectIsToggleAddToCart);
-  const open = Boolean(anchorEl);
-
-  const handleClick = (event: any) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const handleDrawerToggle = () => {
-    setOpenDrawer(!isOpenDrawer);
-  };
-
-  const handleNavigateCartPage = () => {
-    navigation('/cart');
-  };
-
-  const handleNavigation = (link: string) => {
-    if (isOpenDrawer) {
-      handleDrawerToggle();
-    }
-    navigation(link);
-  };
-
-  const handleNavigateClick = () => {
-    handleDrawerToggle();
-    navigation('mentor-profile');
-  };
-
-  useEffect(() => {
-    const handleRefetchCart = () => {
-      if (selectAddToCart) {
-        refetch();
-        dispatch(toggleAddToCart(false));
-      }
-    };
-    handleRefetchCart();
-  }, [dispatch, refetch, selectAddToCart]);
-
   const renderNavigationList = () => {
     return (
       pages &&
@@ -97,18 +109,13 @@ export default function MainNavigation({
         (item) =>
           !item.isHide && (
             <React.Fragment key={item.link}>
-              <NavLink
+              <Button
                 style={{
                   textDecoration: 'none',
                   padding: MetricSize.medium_15,
                 }}
-                to={item.link}
-                onClick={(event) => {
-                  if (item.link.includes('mentor-profile')) {
-                    event.preventDefault();
-                    handleClick(event);
-                  } else if (isOpenDrawer) handleDrawerToggle();
-                }}
+                onClick={() => onClickNavigation(item.link)}
+                onMouseOver={(e) => onMouseEnterNavigation(e, item.link)}
               >
                 <Typography
                   style={{
@@ -121,25 +128,7 @@ export default function MainNavigation({
                 >
                   {item.name}
                 </Typography>
-              </NavLink>
-              {item.link.includes('mentor-profile') && (
-                <Menu
-                  id="basic-menu"
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  MenuListProps={{
-                    'aria-labelledby': 'basic-button',
-                  }}
-                >
-                  <MenuItem onClick={() => handleNavigateClick()}>
-                    Mentor Cuong
-                  </MenuItem>
-                  <MenuItem onClick={() => handleNavigateClick()}>
-                    Mentor Bao
-                  </MenuItem>
-                </Menu>
-              )}
+              </Button>
             </React.Fragment>
           )
       )
@@ -147,89 +136,51 @@ export default function MainNavigation({
   };
 
   return (
-    <Stack sx={SX_NAVIGATION_STACK}>
+    <Stack sx={styles.view}>
       <Stack>
-        <Typography
-          sx={{
-            fontSize: { xs: FontSize.medium_24, md: FontSize.large_45 },
-            fontFamily: FontFamily.bold,
-            color: Color.navy,
-          }}
-        >
-          {localEnvironment.APP_NAME.toUpperCase()}
+        <Typography sx={styles.text1}>
+          {texts.APP_NAME.toUpperCase()}
         </Typography>
       </Stack>
-      <Stack
-        sx={{
-          display: { xs: 'none', md: 'flex' },
-        }}
-        flexDirection="row"
-      >
+      <Stack sx={styles.view1} flexDirection="row">
         {renderNavigationList()}
       </Stack>
-      <Stack
-        sx={{
-          display: { xs: 'none', md: 'flex' },
-        }}
-      >
-        {' '}
+      <Stack sx={styles.view2}>
         {role !== 'TEACHER' && (
-          <IconButton onClick={handleNavigateCartPage}>
+          <IconButton onClick={onClickCart}>
             <Badge
               badgeContent={cart?.totalItem}
               color="secondary"
-              sx={{
-                '& .MuiBadge-badge': {
-                  color: Color.white,
-                  backgroundColor: Color.orange,
-                },
-              }}
+              sx={styles.badge}
             >
               <Icon name="cart" size="medium" />
             </Badge>
           </IconButton>
         )}
       </Stack>
-      <Stack
-        sx={{
-          display: { xs: 'flex', md: 'none' },
-        }}
-      >
-        <IconButton onClick={handleDrawerToggle}>
+      <Stack sx={styles.view3}>
+        <IconButton onClick={onToggleDrawer}>
           <Icon name="menu" size="medium" />
         </IconButton>
       </Stack>
 
-      <Drawer anchor="right" open={isOpenDrawer} onClose={handleDrawerToggle}>
-        <Stack sx={{ padding: MetricSize.medium_15 }}>
-          <Stack
-            sx={{
-              justifyContent: 'space-between',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}
-          >
-            <Typography
-              sx={{
-                fontFamily: FontFamily.bold,
-                fontSize: FontSize.medium_24,
-                color: Color.navy,
-              }}
-            >
-              {localEnvironment.APP_NAME.toUpperCase()}
+      <Drawer anchor="right" open={isOpenDrawer} onClose={onToggleDrawer}>
+        <Stack sx={styles.view}>
+          <Stack sx={styles.subView}>
+            <Typography sx={styles.text2}>
+              {texts.APP_NAME.toUpperCase()}
             </Typography>
-            <IconButton onClick={handleDrawerToggle}>
+            <IconButton onClick={onToggleDrawer}>
               <Icon name="close" color="navy" size="medium" />
             </IconButton>
           </Stack>
           {renderNavigationList()}
           <SearchBar
-            value={filterParams.q || ''}
+            value={filterParams?.q || ''}
             color="black"
-            placeholder="Tìm kiếm khóa học"
+            placeholder={texts.SEARCH_COURSE_PLACEHOLDER}
             onSubmit={onSearchCourse}
           />
-
           <ContractBar color="black" contracts={contracts} />
           <Stack sx={{ justifyContent: 'center', alignItems: 'center' }}>
             <SocialBar color="black" socials={socials} />
@@ -238,15 +189,67 @@ export default function MainNavigation({
               loginData={AuthorizationActionData[0]}
               registerData={AuthorizationActionData[1]}
               onLoginClick={() =>
-                handleNavigation(AuthorizationActionData[0].link)
+                onNavigationLink(AuthorizationActionData[0].link)
               }
               onRegisterClick={() =>
-                handleNavigation(AuthorizationActionData[1].link)
+                onNavigationLink(AuthorizationActionData[1].link)
               }
             />
           </Stack>
         </Stack>
       </Drawer>
+      <Menu
+        id="basic-menu"
+        anchorEl={mentorAnchorEl}
+        open={Boolean(mentorAnchorEl)}
+        onClose={onCloseMentorMenu}
+        onMouseLeave={onMouseLeaveNavigation}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {mentors &&
+          mentors.map((mentor: any) => {
+            return (
+              <MenuItem
+                key={mentor.id}
+                onClick={() =>
+                  onClickNavigation(
+                    `/${texts.INTRODUCE_MENTOR_KEYWORD}/${mentor.id}`
+                  )
+                }
+              >
+                {mentor.name || `Tên giáo viên ${mentor.id}`}
+              </MenuItem>
+            );
+          })}
+      </Menu>
+      <Menu
+        id="basic-menu"
+        anchorEl={courseAnchorEl}
+        open={Boolean(courseAnchorEl)}
+        onClose={onCloseCourseMenu}
+        onMouseLeave={onMouseLeaveNavigation}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        {courses &&
+          courses.map((course: any) => {
+            console.log(course);
+
+            return (
+              <MenuItem
+                key={course.id}
+                onClick={() =>
+                  onClickNavigation(`/${texts.COURSE_MENU_LINK}/${course.id}`)
+                }
+              >
+                {course.title || `Tên khóa học ${course.id}`}
+              </MenuItem>
+            );
+          })}
+      </Menu>
     </Stack>
   );
 }
