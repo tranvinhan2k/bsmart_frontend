1950fb342e1fd45642e1de44d5d2976a79f8236d
luuquangnhat <nhatluu9991@gmail.com>
2023-05-18 18:54:02 +0700

    misc-convention-fix missing routes

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -138,6 +138,54 @@ export const NavigationActionData: ActionPayload[] = [
     name: 'Xác nhận khóa học',
     link: 'confirm_email',
   },
+  {
+    id: 20,
+    isHide: true,
+    name: 'Quản lý Resource',
+    link: 'mentor-resource-management',
+  },
+  {
+    id: 21,
+    isHide: true,
+    name: 'Quản lý Quiz',
+    link: 'mentor-quiz-settings',
+  },
+  {
+    id: 22,
+    isHide: true,
+    name: 'Giảng viên điểm danh',
+    link: 'mentor-take-attendance',
+  },
+  {
+    id: 23,
+    isHide: true,
+    name: 'Giảng viên điểm danh lại',
+    link: 'mentor-re-take-attendance',
+  },
+  {
+    id: 24,
+    isHide: true,
+    name: 'Danh sách tài khoản cần phê duyệt',
+    link: 'admin-register-approve',
+  },
+  {
+    id: 25,
+    isHide: true,
+    name: 'Admin phê duyệt tạo tài khoản',
+    link: 'admin-register-approve/:id',
+  },
+  {
+    id: 26,
+    isHide: true,
+    name: 'Danh sách khóa học cần phê duyệt',
+    link: 'admin-create-course-request-approve',
+  },
+  {
+    id: 27,
+    isHide: true,
+    name: 'Admin phê duyệt tạo khóa học',
+    link: 'admin-create-course-request-approve/:id',
+  },
 ];
 
 export const MentorNavigationActionData: ActionPayload[] = [
