0cd59a5f856d1ada0dc762b7b4714e389f353899
luuquangnhat <nhatluu9991@gmail.com>
2023-06-29 01:41:46 +0700

    all-ui-add info in about us sec of home page

--- src/containers/HomeSection/SectionAboutUs/index.tsx
+++ src/containers/HomeSection/SectionAboutUs/index.tsx
@@ -1,36 +1,60 @@
 import { Box, Grid, Stack, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { AuthorizationActionData, NavigationActionData } from '~/constants';
+import { RootState } from '~/redux/store';
 import Button from '~/components/atoms/Button';
 import img_banner_sub_typing_1 from '~/assets/images/HomePageSection/img_banner_sub_typing_1.jpg';
 import { SX } from './style';
 
 export default function SectionAboutUs() {
+  const navigate = useNavigate();
+  const handleViewCourse = () => navigate(`/${NavigationActionData[2].link}`);
+  const handleRegister = () => navigate(AuthorizationActionData[1].link);
+
+  const token =
+    useSelector((state: RootState) => state.user.token) ||
+    localStorage.getItem('token');
+
   return (
     <Box sx={SX.BOX_BANNER}>
       <Box sx={SX.CONTAINER} px={16}>
         <Typography component="h2" sx={SX.H2}>
           Về chúng tôi
         </Typography>
-        <Grid container spacing={2}>
+        <Grid container spacing={6}>
           <Grid item xs={12} md={6}>
-            <Typography component="p" sx={SX.P}>
-              BSmart khai thác nhu cầu tuyển dụng lập trình, kết nối việc làm
-              tới doanh nghiệp và tích hợp các dự án với công nghệ mới nhất vào
-              phương pháp đào tạo tích cực cho các học viên học xong có việc làm
-              ngay. Chương trình giảng dạy được biên soạn may đo dành riêng cho
-              các bạn Lập trình từ trái ngành hoặc đã có kiến thức theo cường độ
-              cao, luôn được tinh chỉnh và tối ưu hóa theo thời gian bởi các
-              thành viên sáng lập và giảng viên dày kinh nghiệm.
-            </Typography>
+            <Box>
+              <Typography component="p" sx={SX.P}>
+                Bước vào thế giới của công nghệ với Trung tâm Dạy Lập Trình hàng
+                đầu! Chúng tôi mang đến cho bạn khóa học lập trình chất lượng,
+                với môi trường học tập đa dạng và giảng viên tận tâm.
+              </Typography>
+            </Box>
+            <Box mt={4}>
+              <Typography component="p" sx={SX.P}>
+                Hãy tham gia ngay để nắm bắt kiến thức và kỹ năng cần thiết để
+                trở thành một lập trình viên tài năng. Chúng tôi cam kết hỗ trợ
+                bạn trên con đường phát triển nghề nghiệp và tìm kiếm cơ hội
+                việc làm. Đăng ký ngay và khám phá tiềm năng lập trình của bạn
+                với chúng tôi!
+              </Typography>
+            </Box>
             <Stack
               direction={{ xs: 'column', sm: 'row' }}
               justifyContent="center"
               alignItems="center"
               spacing={1}
-              /*  */
               mt={6}
             >
-              <Button customVariant="normal">Xem khoá học</Button>
-              <Button customVariant="outlined">Hỗ trợ tư vấn</Button>
+              <Button customVariant="normal" onClick={handleViewCourse}>
+                Xem khoá học
+              </Button>
+              {Boolean(!token) && (
+                <Button customVariant="outlined" onClick={handleRegister}>
+                  Đăng ký ngay
+                </Button>
+              )}
             </Stack>
           </Grid>
           <Grid item xs={12} md={6}>
