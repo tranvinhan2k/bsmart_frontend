fc155d4344f1b6eae2e1654d55b732fc907f2f2f
tranvinhan <tranvinhan2k@gmail.com>
2023-07-05 10:39:18 +0700

    update UI for dashboard sidebar

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
 
-import { Grid, Stack, Typography, Box } from '@mui/material';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Stack, Typography, Drawer, IconButton, Box } from '@mui/material';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { scrollToTop } from '~/utils/common';
 import { Color, FontFamily, FontSize, MetricSize } from '~/assets/variables';
-import Icon, { IconName } from '~/components/atoms/Icon';
+import Icon from '~/components/atoms/Icon';
 import { mentorLMSRoutes } from '~/routes';
 import { RoutePayload } from '~/models/routes';
 import { MentorDashboardNavigationActionData } from '~/constants';
 import DashboardSidebarButton from '~/components/molecules/DashboardSidebarButton';
-import { image } from '~/constants/image';
 import { NavigationLink } from '~/constants/routeLink';
+import DashboardBreadcrumbNavigation from '~/components/molecules/navigations/DashboardBreadcrumbNavigation';
+import Button from '~/components/atoms/Button';
 
 export default function DashboardPage() {
   React.useEffect(() => {
@@ -18,8 +19,15 @@ export default function DashboardPage() {
   }, []);
 
   const navigate = useNavigate();
+  const { pathname } = useLocation();
 
   const [activeIndex, setActiveIndex] = useState(0);
+  const [isHover, setHover] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
 
   const handleSetActiveIndex = (idx: number) => {
     setActiveIndex(idx);
@@ -50,116 +58,36 @@ export default function DashboardPage() {
   const rows = MentorDashboardNavigationActionData;
 
   return (
-    <Grid
-      container
+    <Stack
       sx={{
+        flexDirection: { xs: 'column', md: 'row' },
         position: 'relative',
         minHeight: '100vh',
         background: Color.white4,
       }}
     >
-      <Grid
-        sx={{
-          background: Color.navy,
-        }}
-        item
-        xs={12}
-        md={2}
-      >
+      <>
         <Stack
           sx={{
-            position: 'sticky',
-            top: 0,
-            zIndex: 9,
-            background: Color.white4,
-            shadow: 3,
+            background: Color.navy,
+            padding: 1,
+            display: { xs: 'flex', md: 'none' },
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
           }}
         >
-          <Stack
-            sx={{
-              paddingTop: 3,
-              background: Color.navy,
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: FontSize.small_18,
-                fontFamily: FontFamily.bold,
-                color: Color.white,
-                textAlign: 'center',
-              }}
-            >
-              LMS
-            </Typography>
-          </Stack>
-          <Stack
-            sx={{
-              transition: 'height 500ms ease',
-              borderBottomRightRadius:
-                activeIndex === 0 ? MetricSize.small_10 : 0,
-              padding: MetricSize.medium_15,
-              fontSize: FontSize.small_18,
-              fontFamily: FontFamily.regular,
-              background: Color.navy,
-            }}
-          />
-          {rows?.map((item, index) => {
-            return (
-              <DashboardSidebarButton
-                activeIndex={activeIndex}
-                index={index}
-                item={item}
-                onNavigateLink={handleNavigateLink}
-                onSetActive={handleSetActiveIndex}
-                key={item.id}
-              />
-            );
-          })}
-          <Stack
-            sx={{
-              transition: 'background 200ms',
-              borderTopRightRadius:
-                activeIndex === rows.length - 1 ? MetricSize.small_10 : 0,
-              padding: MetricSize.medium_15,
-              fontSize: FontSize.small_18,
-              fontFamily: FontFamily.regular,
-              background: Color.navy,
-            }}
-          />
-          <Stack sx={{ background: Color.navy }}>
-            <Stack
+          <Box>
+            <IconButton onClick={handleOpen}>
+              <Icon name="menu" size="medium" color="white" />
+            </IconButton>
+          </Box>
+          <Box>
+            <Button
               onClick={handleNavigateHomepage}
-              sx={{
-                position: 'relative',
-                margin: 1,
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-                transition: 'all 0.5s ease',
-                background: `${Color.orange}AA`,
-                padding: 1,
-                backdropFilter: 'blur(2px)',
-                borderRadius: MetricSize.small_5,
-                fontFamily: FontFamily.light,
-                fontSize: FontSize.small_14,
-                zIndex: 10,
-                color: Color.white,
-
-                ':hover': {
-                  background: `${Color.orange}`,
-                  backdropFilter: 'blur(0px)',
-                  cursor: 'pointer',
-                },
-              }}
+              customVariant="form"
+              startIcon={<Icon name="return" size="small_20" color="white" />}
             >
-              <Stack
-                sx={{
-                  position: 'absolute',
-                  left: MetricSize.small_10,
-                }}
-              >
-                <Icon name="return" size="small_20" color="white" />
-              </Stack>
               <Typography
                 sx={{
                   marginLeft: 1,
@@ -167,13 +95,268 @@ export default function DashboardPage() {
               >
                 Trở về trang chủ
               </Typography>
+            </Button>
+          </Box>
+        </Stack>
+        <Drawer
+          sx={{
+            ':-webkit-scrollbar': {
+              display: 'none',
+            },
+          }}
+          anchor="left"
+          open={open}
+          onClose={handleOpen}
+        >
+          <Stack
+            sx={{
+              background: Color.navy,
+              flex: 1,
+            }}
+          >
+            <Stack
+              sx={{
+                paddingTop: 3,
+                background: Color.navy,
+              }}
+            >
+              <Typography
+                sx={{
+                  fontSize: FontSize.small_18,
+                  fontFamily: FontFamily.bold,
+                  color: Color.white,
+                  textAlign: 'center',
+                }}
+              >
+                LMS
+              </Typography>
+            </Stack>
+            <Stack
+              sx={{
+                transition: 'all 1000ms ease',
+                borderBottomRightRadius:
+                  activeIndex === 0 ? MetricSize.small_10 : 0,
+                padding: MetricSize.medium_15,
+                fontSize: FontSize.small_18,
+                fontFamily: FontFamily.regular,
+                background: Color.navy,
+              }}
+            />
+            {rows?.map((item, index) => {
+              return (
+                <DashboardSidebarButton
+                  isHover
+                  activeIndex={activeIndex}
+                  index={index}
+                  item={item}
+                  onNavigateLink={handleNavigateLink}
+                  onSetActive={handleSetActiveIndex}
+                  key={item.id}
+                />
+              );
+            })}
+            <Stack
+              sx={{
+                transition: 'background 1000ms',
+                borderTopRightRadius:
+                  activeIndex === rows.length - 1 ? MetricSize.small_10 : 0,
+                padding: MetricSize.medium_15,
+                fontSize: FontSize.small_18,
+                fontFamily: FontFamily.regular,
+                background: Color.navy,
+              }}
+            />
+            <Stack sx={{ background: Color.navy }}>
+              <Stack
+                onClick={handleNavigateHomepage}
+                sx={{
+                  display: isHover ? 'flex' : 'none',
+                  position: 'relative',
+                  margin: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  transition: 'all 1000ms ease',
+                  background: `${Color.orange}AA`,
+                  padding: 1,
+                  backdropFilter: 'blur(2px)',
+                  borderRadius: MetricSize.small_5,
+                  fontFamily: FontFamily.light,
+                  fontSize: FontSize.small_14,
+                  zIndex: 10,
+                  color: Color.white,
+
+                  ':hover': {
+                    background: `${Color.orange}`,
+                    backdropFilter: 'blur(0px)',
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                <Stack
+                  sx={{
+                    position: 'absolute',
+                    left: MetricSize.small_10,
+                  }}
+                >
+                  <Icon name="return" size="small_20" color="white" />
+                </Stack>
+                <Typography
+                  sx={{
+                    marginLeft: 1,
+                  }}
+                >
+                  Trở về trang chủ
+                </Typography>
+              </Stack>
             </Stack>
           </Stack>
+        </Drawer>
+        <Stack
+          sx={{
+            display: { xs: 'none', md: 'flex' },
+            marginLeft: '60px',
+          }}
+        >
+          <Stack
+            onMouseOut={() => setHover(false)}
+            onMouseOver={() => setHover(true)}
+            sx={{
+              transition: 'all 1000ms ease',
+              background: Color.white4,
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              zIndex: 9,
+              shadow: 3,
+              maxWidth: isHover ? '100%' : '60px',
+              height: '100vh',
+            }}
+          >
+            <Stack
+              sx={{
+                paddingTop: 3,
+                background: Color.navy,
+              }}
+            >
+              <Typography
+                sx={{
+                  fontSize: FontSize.small_18,
+                  fontFamily: FontFamily.bold,
+                  color: Color.white,
+                  textAlign: 'center',
+                }}
+              >
+                LMS
+              </Typography>
+            </Stack>
+            <Stack
+              sx={{
+                transition: 'all 1000ms ease',
+                borderBottomRightRadius: {
+                  xs: 0,
+                  md: activeIndex === 0 ? MetricSize.small_10 : 0,
+                },
+                padding: MetricSize.medium_15,
+                fontSize: FontSize.small_18,
+                fontFamily: FontFamily.regular,
+                background: Color.navy,
+              }}
+            />
+            {rows?.map((item, index) => {
+              return (
+                <DashboardSidebarButton
+                  isHover={isHover}
+                  activeIndex={activeIndex}
+                  index={index}
+                  item={item}
+                  onNavigateLink={handleNavigateLink}
+                  onSetActive={handleSetActiveIndex}
+                  key={item.id}
+                />
+              );
+            })}
+            <Stack
+              sx={{
+                transition: 'background 1000ms',
+                borderTopRightRadius: {
+                  xs: 0,
+                  md: activeIndex === rows.length - 1 ? MetricSize.small_10 : 0,
+                },
+
+                padding: MetricSize.medium_15,
+                fontSize: FontSize.small_18,
+                fontFamily: FontFamily.regular,
+                background: Color.navy,
+              }}
+            />
+            <Stack sx={{ background: Color.navy }}>
+              <Stack
+                onClick={handleNavigateHomepage}
+                sx={{
+                  display: isHover ? 'flex' : 'none',
+                  position: 'relative',
+                  margin: 1,
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  transition: 'all 1000ms ease',
+                  background: `${Color.orange}AA`,
+                  padding: 1,
+                  backdropFilter: 'blur(2px)',
+                  borderRadius: MetricSize.small_5,
+                  fontFamily: FontFamily.light,
+                  fontSize: FontSize.small_14,
+                  zIndex: 10,
+                  color: Color.white,
+
+                  ':hover': {
+                    background: `${Color.orange}`,
+                    backdropFilter: 'blur(0px)',
+                    cursor: 'pointer',
+                  },
+                }}
+              >
+                <Stack
+                  sx={{
+                    position: 'absolute',
+                    left: MetricSize.small_10,
+                  }}
+                >
+                  <Icon name="return" size="small_20" color="white" />
+                </Stack>
+                <Typography
+                  sx={{
+                    marginLeft: 1,
+                  }}
+                >
+                  Trở về trang chủ
+                </Typography>
+              </Stack>
+            </Stack>
+            <Stack
+              sx={{
+                flex: 1,
+                background: Color.navy,
+                flexGrow: 1,
+              }}
+            />
+          </Stack>
         </Stack>
-      </Grid>
-      <Grid item xs={12} md={10} padding={3}>
+      </>
+
+      <Stack
+        sx={{
+          padding: {
+            xs: MetricSize.small_10,
+            md: 5,
+          },
+          flexGrow: 1,
+        }}
+      >
+        <DashboardBreadcrumbNavigation />
         <Routes>{showDashboardMentorRoutes()}</Routes>
-      </Grid>
-    </Grid>
+      </Stack>
+    </Stack>
   );
 }
