c27634026af5f59f2960bddfcf3e4cb66d4a3737
tranvinhan <tranvinhan2k@gmail.com>
2023-08-24 16:03:23 +0700

    update facebook plugin

--- src/containers/HomeSection/index.tsx
+++ src/containers/HomeSection/index.tsx
@@ -21,43 +21,38 @@ export default function HomePageSection() {
       <SectionContactUs />
       <Stack
         dangerouslySetInnerHTML={{
-          __html: `
-        <div id="fb-root"></div>
-    
-        <div id="fb-customer-chat" class="fb-customerchat"></div>
-    
+          __html: `<div id="fb-root" />
+        <div id="fb-customer-chat" className="fb-customerchat" />
         <script>
-          if (window.location.pathname.includes('homepage')) {
-            var chatbox = document.getElementById('fb-customer-chat');
-            chatbox.setAttribute('page_id', '107932629047954');
-            chatbox.setAttribute('attribution', 'biz_inbox');
-          }
-        </script>
-    
-        <!-- Your SDK code -->
-        <script>
-          if (window.location.pathname.includes('homepage')) {
-            window.fbAsyncInit = function () {
-              FB.init({
-                xfbml: true,
-                version: 'v17.0',
-              });
-            };
-    
-            (function (d, s, id) {
-              var js,
-                fjs = d.getElementsByTagName(s)[0];
-              if (d.getElementById(id)) return;
-              js = d.createElement(s);
-              js.id = id;
-              js.src =
-                'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-              fjs.parentNode.insertBefore(js, fjs);
-            })(document, 'script', 'facebook-jssdk');
-          }
-        </script>
-    
-        <script type="module" src="/src/main.tsx"></script>`,
+        if (window.location.pathname.includes('homepage')) {
+          var chatbox = document.getElementById('fb-customer-chat');
+          chatbox.setAttribute('page_id', '107932629047954');
+          chatbox.setAttribute('attribution', 'biz_inbox');
+        }
+      </script>
+  
+     
+      <script>
+        if (window.location.pathname.includes('homepage')) {
+          window.fbAsyncInit = function () {
+            FB.init({
+              xfbml: true,
+              version: 'v17.0',
+            });
+          };
+  
+          (function (d, s, id) {
+            var js,
+              fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src =
+              'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+          })(document, 'script', 'facebook-jssdk');
+        }
+      </script>`,
         }}
       />
     </>
