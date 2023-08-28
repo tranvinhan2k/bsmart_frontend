7dad79bd84d4bcd106809872f5a302f8be72675a
tranvinhan <tranvinhan2k@gmail.com>
2023-08-28 16:35:29 +0700

    update check role

--- src/hooks/mentorProfile/useCheckCompleteness.ts
+++ src/hooks/mentorProfile/useCheckCompleteness.ts
@@ -5,7 +5,7 @@ import { useDispatchProfile } from '../useDispatchProfile';
 
 export const useCheckCompleteness = () => {
   const { profile } = useDispatchProfile();
-  const role = profile?.roles?.[0].code;
+  const role = profile?.roles?.[0]?.code;
   const { data, isLoading, isError, error, refetch } = useQuery({
     queryKey: [keyMentorProfileUseCheckCompleteness],
     queryFn: () => mentorProfilesApi.checkCompleteness(),
