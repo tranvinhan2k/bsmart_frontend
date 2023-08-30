8d6b010d27fe6787b09b2a55f3cbcd9bd76fe58e
luuquangnhat <nhatluu9991@gmail.com>
2023-08-30 09:35:48 +0700

    manager-logic-hot fix request edit profile

--- src/components/molecules/ManageTableUser/index.tsx
+++ src/components/molecules/ManageTableUser/index.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
-import { ManagedMentorPayload } from '~/models/type';
 import { MenuItemPayload } from '~/components/molecules/CRUDTable';
 import { rowsPerPageOptionsDefault } from '~/constants/dataGrid';
 import { useSearchManagedUser } from '~/hooks/user/useSearchManagedUser';
 import columns from '~/constants/columns';
 import CustomDialog from '~/components/atoms/CustomDialog';
-import CustomModal from '~/components/atoms/CustomModal';
 import ManageTable from '~/components/molecules/ManageTable';
 import ManageTableDetailsManagedMember from '~/components/molecules/ManageTableDetailsManagedMember';
 import ManageTableDetailsManagedMentor from '~/components/molecules/ManageTableDetailsManagedMentor';
-import ManageTableDetailsManagedUserRevenue from '../ManageTableDetailsManagedUserRevenue';
+import { ManagedMentorPayload } from '~/models/type';
+import CustomModal from '~/components/atoms/CustomModal';
+import { useMutationProcessManagerRequestEditMentor } from '~/hooks/user/useMutationProcessManagerRequestEditMentor';
+import toast from '~/utils/toast';
+import { toastMsgError } from '~/utils/common';
 
 interface ManageTableUserProps {
   userRole: 'TEACHER' | 'STUDENT';
@@ -26,6 +28,7 @@ export default function ManageTableUser({
     optionNotSupport = 'Chưa hỗ trợ',
     optionViewDetails = 'Xem chi tiết',
     optionViewRevenue = 'Xem doanh thu',
+    optionRequestEdit = 'Yêu cầu chỉnh sửa',
   }
   const [open, setOpen] = useState<boolean>(false);
   const [mode, setMode] = useState<'READ' | 'REVENUE' | ''>('');
@@ -65,6 +68,28 @@ export default function ManageTableUser({
     setMode(() => 'REVENUE');
   };
 
+  const { processManagerRequestEditMentor } =
+    useMutationProcessManagerRequestEditMentor();
+
+  const toastMsgLoading = 'Đang xử lý...';
+  const toastMsgSuccess = 'Xử lý thành công';
+  const handleRequestEdit = async () => {
+    // console.log('selectedRow.mentorProfile.id', selectedRow.mentorProfile.id);
+    if (selectedRow) {
+      const id = toast.loadToast(toastMsgLoading);
+      try {
+        await processManagerRequestEditMentor.mutateAsync(
+          selectedRow.mentorProfile.id
+        );
+        refetchGetNoOfRequest();
+
+        toast.updateSuccessToast(id, toastMsgSuccess);
+      } catch (e: unknown) {
+        toast.updateFailedToast(id, toastMsgError(e));
+      }
+    }
+  };
+
   const optionListDefault: MenuItemPayload[] = [
     {
       icon: 'question',
@@ -72,7 +97,24 @@ export default function ManageTableUser({
       onCLick: () => {},
     },
   ];
-  const optionListViewDetails: MenuItemPayload[] = [
+  const optionListViewDetailsMentor: MenuItemPayload[] = [
+    {
+      icon: 'category',
+      title: Text.optionViewDetails,
+      onCLick: handleOpenManagedUserDetails,
+    },
+    {
+      icon: 'biMoney',
+      title: Text.optionViewRevenue,
+      onCLick: handleOpenManageRevenue,
+    },
+    {
+      icon: 'edit',
+      title: Text.optionRequestEdit,
+      onCLick: handleRequestEdit,
+    },
+  ];
+  const optionListViewDetailsStudent: MenuItemPayload[] = [
     {
       icon: 'category',
       title: Text.optionViewDetails,
@@ -90,12 +132,12 @@ export default function ManageTableUser({
   let renderSearchPlaceholder;
   switch (userRole) {
     case 'TEACHER':
-      renderOptions = optionListViewDetails;
+      renderOptions = optionListViewDetailsMentor;
       renderColumns = columns.managedUserMentorColumns;
       renderSearchPlaceholder = Text.searchPlaceholderMentor;
       break;
     case 'STUDENT':
-      renderOptions = optionListViewDetails;
+      renderOptions = optionListViewDetailsStudent;
       renderColumns = columns.managedUserMemberColumns;
       renderSearchPlaceholder = Text.searchPlaceholderMember;
       break;
@@ -135,12 +177,13 @@ export default function ManageTableUser({
       break;
     case 'REVENUE':
       renderItem = (
-        <CustomDialog open={open} onClose={handleTriggerDialog} maxWidth="sm">
-          <ManageTableDetailsManagedUserRevenue
-            rowId={selectedRow && selectedRow.id}
-            userRole={userRole}
-          />
-        </CustomDialog>
+        <CustomModal
+          open={open}
+          onClose={handleTriggerDialog}
+          title="Doanh thu của người dùng"
+        >
+          Doanh Thu
+        </CustomModal>
       );
       break;
     default:
